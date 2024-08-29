1478c3bf3fb22d4301fcaa835d7db81d6b38bce6
victorfs32 <jvfds32@gmail.com>
2024-08-29 11:44:51 -0300

    ATUALIZANDO

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Caminho para o arquivo de resultados
-const resultadosPath = path.join(__dirname, 'resultados.json');
+// Caminho para o arquivo de pontuações
+const scoresFilePath = path.join(__dirname, 'resultados.json');
 
 // Configura o CORS para permitir requisições do domínio do frontend
 app.use(cors({
@@ -15,35 +15,30 @@ app.use(cors({
 
 app.use(express.json()); // Para interpretar JSON
 
-// Função para ler o arquivo de resultados
+// Função para ler as pontuações do arquivo
 const readScoresFromFile = () => {
-  try {
-    if (fs.existsSync(resultadosPath)) {
-      const fileData = fs.readFileSync(resultadosPath);
-      return JSON.parse(fileData);
-    } else {
-      return [];
-    }
-  } catch (error) {
-    console.error('Erro ao ler o arquivo de resultados:', error);
+  if (fs.existsSync(scoresFilePath)) {
+    const data = fs.readFileSync(scoresFilePath, 'utf8');
+    return JSON.parse(data);
+  } else {
     return [];
   }
 };
 
-// Função para escrever no arquivo de resultados
+// Função para escrever as pontuações no arquivo
 const writeScoresToFile = (scores) => {
-  try {
-    fs.writeFileSync(resultadosPath, JSON.stringify(scores, null, 2));
-    console.log('Pontuações salvas com sucesso!');
-  } catch (error) {
-    console.error('Erro ao escrever no arquivo de resultados:', error);
-  }
+  fs.writeFileSync(scoresFilePath, JSON.stringify(scores, null, 2), 'utf8');
 };
 
 // Rota para obter as pontuações
 app.get('/api/ranking', (req, res) => {
-  const scores = readScoresFromFile();
-  res.json(scores);
+  try {
+    const scores = readScoresFromFile();
+    res.json(scores);
+  } catch (error) {
+    console.error('Erro ao obter pontuações:', error);
+    res.status(500).json({ message: 'Erro ao obter pontuações!' });
+  }
 });
 
 // Rota para adicionar uma nova pontuação
