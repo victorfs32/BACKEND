b4f7c3019345173f0952288650adcfc5e027bffa
victorfs32 <jvfds32@gmail.com>
2024-08-29 11:14:57 -0300

    ATUALIZANDO

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -2,59 +2,51 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const resultsFilePath = path.join(__dirname, 'results.json');
 
 // Configura o CORS para permitir requisições do domínio do frontend
 app.use(cors({
   origin: 'https://www.ensinandolibras.com.br',
 }));
 
-app.use(express.json()); // Para parsear JSON no corpo das requisições
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// Endpoint para salvar as pontuações
+// Função para ler resultados do arquivo JSON
+const readResultsFromFile = () => {
+  if (fs.existsSync(resultsFilePath)) {
+    const data = fs.readFileSync(resultsFilePath, 'utf8');
+    return JSON.parse(data);
+  }
+  return [];
+};
+
+// Função para salvar resultados no arquivo JSON
+const saveResultsToFile = (results) => {
+  fs.writeFileSync(resultsFilePath, JSON.stringify(results, null, 2), 'utf8');
+};
+
+// Endpoint para salvar os resultados
 app.post('/scores', (req, res) => {
   const { userName, score, timeTaken } = req.body;
-
-  if (!userName || score === undefined || timeTaken === undefined) {
-    return res.status(400).json({ error: 'Dados inválidos' });
-  }
-
-  const filePath = path.join(__dirname, 'resultados.json');
-  console.log(`Lendo e escrevendo no arquivo: ${filePath}`);
-
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err && err.code !== 'ENOENT') {
-      console.error('Erro ao ler o arquivo:', err);
-      return res.status(500).json({ error: 'Erro ao ler o arquivo' });
-    }
-
-    let scores = [];
-    if (data) {
-      try {
-        scores = JSON.parse(data);
-      } catch (parseErr) {
-        console.error('Erro ao parsear os dados do arquivo:', parseErr);
-        return res.status(500).json({ error: 'Erro ao processar os dados' });
-      }
-    }
-
-    scores.push({ userName, score, timeTaken });
-
-    fs.writeFile(filePath, JSON.stringify(scores, null, 2), (err) => {
-      if (err) {
-        console.error('Erro ao salvar o arquivo:', err);
-        return res.status(500).json({ error: 'Erro ao salvar o arquivo' });
-      }
-      res.status(201).json({ message: 'Pontuação salva com sucesso' });
-    });
-  });
+  const results = readResultsFromFile();
+  results.push({ userName, score, timeTaken });
+  saveResultsToFile(results);
+  res.status(201).send('Score saved');
 });
 
-// Endpoint para verificar a conexão
+// Endpoint para recuperar resultados
+app.get('/scores', (req, res) => {
+  const results = readResultsFromFile();
+  res.json(results);
+});
+
 app.get('/', (req, res) => {
-  res.send('Servidor está funcionando!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
