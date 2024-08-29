d05e486e039dd29d2ee1b95054884594d14049bf
victorfs32 <jvfds32@gmail.com>
2024-08-29 08:31:46 -0300

    UP

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configura o diretório para arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
