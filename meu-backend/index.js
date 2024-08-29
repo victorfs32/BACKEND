8a9bee144f07556a158afa4c4308c94585c2d375
victorfs32 <jvfds32@gmail.com>
2024-08-29 08:28:48 -0300

    UP

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -1,36 +1,11 @@
 const express = require('express');
-const cors = require('cors'); // Importar o módulo cors
 const app = express();
-app.use(express.json());
-
-// Configurar o middleware CORS
-const corsOptions = {
-    origin: 'https://www.ensinandolibras.com.br', // Seu domínio
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
-};
-app.use(cors(corsOptions));
-
-let quizResults = [];
-
-// Rota POST para salvar os resultados do quiz
-app.post('/save-score', (req, res) => {
-    const { userName, score, timeTaken } = req.body;
-
-    // Armazenar o resultado (em memória para este exemplo)
-    quizResults.push({ userName, score, timeTaken });
-
-    res.status(200).send('Score saved successfully');
-});
-
-// Rota GET para obter o ranking
-app.get('/ranking', (req, res) => {
-    // Ordena os resultados por pontuação e tempo
-    quizResults.sort((a, b) => b.score - a.score || a.timeTaken - b.timeTaken);
-    res.json(quizResults);
-});
-
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
