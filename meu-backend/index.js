5242717147b8580cd0b69f95ba0882b15971dd44
victorfs32 <jvfds32@gmail.com>
2024-08-29 09:01:54 -0300

    ATUALIZADO

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configura o CORS para permitir requisições do frontend
+// Configura o CORS para permitir requisições do domínio do frontend
 app.use(cors({
-  origin: 'https://www.ensinandolibras.com.br'
+  origin: 'https://www.ensinandolibras.com.br',
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
