fcd6d75dc4a545b6bf209d58a8c8bc90a84fec1e
victorfs32 <jvfds32@gmail.com>
2024-08-29 08:49:20 -0300

    ATUALIZADO

--- meu-backend/index.js
+++ meu-backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configura o diretório para arquivos estáticos
+// Configura o CORS para permitir requisições do frontend
+app.use(cors({
+  origin: 'https://www.ensinandolibras.com.br'
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
